39bf65a520ac7d64b724c2622aef055532adb54e
Sergey Kipnis <skipnis@gmail.com>
2024-02-16 13:44:36 +0100

    update

--- python/write_file.py
+++ python/write_file.py
@@ -2,11 +2,10 @@ with open("/tmp/my_log.txt", "a") as f:
     # Writing data to a file
     f.write("xxxxx \n")
 
-def print_any(sd: Any, mark: str = "---") -> ServiceData:
+def print_any(sd: Any, mark: str = "---"):
     TO_FILE = "/tmp/sd.txt"
     with open(TO_FILE, "a") as f:
         f.write(f"{mark} '{sd}'\n")
-    return sd
 
 def print_f(value: str) :
     TO_FILE = "/tmp/sd.txt"
